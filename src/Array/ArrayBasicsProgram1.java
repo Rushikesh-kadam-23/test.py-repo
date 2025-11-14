eea338e9ea1907ecd2dffca405bc6b637a13aefe
Rushikesh Kadam <kadamrushikeshg6@gmail.com>
2025-11-14 14:58:12 +0530

    message

---
+++ src/Array/ArrayBasicsProgram1.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class ArrayBasicsProgram1 {
+
+        public static void main(String[] args) {
+
+            int[][] a = {{10,20,30},{40,50},{60,70,80}};
+
+            for(int i=0;i<a.length;i++){
+                for(int j=0;j<a[i].length;j++){
+                    System.out.print(a[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+    public static class ArrayBasicsProgram {
+
+        public static void main(String[] args) {
+
+            int [] a= {10,20,30,40,50};
+
+            for(int i=0;i<a.length;i++){
+                System.out.println(a[i]);
+
+            }
+        }
+
+
+
+    }
+}
+
+
